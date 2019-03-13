029b59569111c28b93d49eaea13f00880e7f0f1e
gogbog <gogbog11@gmail.com>
2019-03-13 08:59:34 +0200

    Menu logic added

---
+++ src/Helpers/AdministrationMenu.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Charlotte\Administration\Helpers;
+
+
+class AdminMenu {
+
+    //name
+    //route
+    //icon
+
+    public static function addModule($data= []) {
+
+    }
+
+    public static function addItem($data = []) {
+
+    }
+
+
+//\AdministrationMenu::addModule(trans('blogs::admin.module_name'), [
+//'icon' => 'newspaper-o'
+//], function ($menu) {
+//    $menu->addItem(trans('blogs::admin.list'), [
+//        'url' => \Administration::route('blogs.index'),
+//        'icon' => 'list'
+//    ]);
+//
+//    $menu->addItem(trans('blogs::admin.add'), [
+//        'url' => \Administration::route('blogs.create'),
+//        'icon' => 'plus'
+//    ]);
+//
+//    $menu->addItem(trans('blogs::admin.blog_categories'), [
+//        'icon' => 'arrow-right'
+//    ], function ($submenu)
+//    {
+//        $submenu->addItem(trans('blogs::admin.list'), [
+//            'url' => \Administration::route('blog_categories.index'),
+//            'icon' => 'list'
+//        ]);
+//
+//        $submenu->addItem(trans('blogs::admin.add'), [
+//            'url' => \Administration::route('blog_categories.create'),
+//            'icon' => 'plus'
+//        ]);
+//    });
+//});
+
+}
