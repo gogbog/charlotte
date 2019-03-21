2e3c3c5848e0797e8585248fe2b3ae05f41e91c3
Deyan Bozhilov <dodobrat98@gmail.com>
2019-03-21 16:27:00 +0200

    adding separate js

--- resources/views/boxes/nav_links.blade.php
+++ resources/views/boxes/nav_links.blade.php
@@ -12,10 +12,10 @@
             $active = true;
         }
     @endphp
-    <li class="@if(!empty($item->attr()['class'])) {{ $item->attr()['class'] }} @endif @if($item->hasChildren()) dropdown @endif  ">
+    <li class="@if(!empty($item->attr()['class'])) {{ $item->attr()['class'] }} @endif @if($item->hasChildren()) dropdown @endif">
         @if(empty($item->attr()['global']))
             <a href="@if (!empty($item->url())){!! $item->url() !!}@else javascript:void(0)  @endif"
-               class="waves-effect @if ($active) active @endif">
+               class="@if ($active) active @endif">
                 <i class="fa fa-fw linea-icon linea-basic m-r-10 @if (!empty($item->attr()['icon'])) {{ $item->attr()['icon'] }} @endif"></i>
                 <span class="hide-menu">
                 {!! $item->title !!}
@@ -26,7 +26,7 @@
             {!! $item->title !!}
         @endif
         @if($item->hasChildren())
-            <ul class="nav nav-second-level" style="padding-left: 30px;">
+            <ul class="nav nav-second-level">
                 @include('administration::boxes.nav_links', ['items' => $item->children()])
             </ul>
         @endif
--- resources/views/layouts/administration-master.blade.php
+++ resources/views/layouts/administration-master.blade.php
@@ -27,7 +27,7 @@
     <script src="{{ asset(config('administration.file_prefix') . 'js/respond.min.js') }}"></script>
     <![endif]-->
 </head>
-<body class="fix-header fix-sidebar" onload="startTime()">
+<body class="fix-header fix-sidebar">
 <!-- Preloader -->
 <div class="preloader">
     <div class="cssload-speeding-wheel"></div>
@@ -229,66 +229,7 @@
 <script src="{{ asset(config('administration.file_prefix') . 'js/charts.js') }}"></script>
 <script src="{{ asset(config('administration.file_prefix') . 'js/editor.js') }}"></script>
 <script src="{{ asset(config('administration.file_prefix') . 'js/data-table.js') }}"></script>
-<script>
-
-    let mode = document.getElementById('myonoffswitch');
-
-    mode.addEventListener('click', function () {
-
-        let checked = mode.checked;
-        let url = mode.dataset.url;
-
-        $.ajaxSetup({
-            cache: false,
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-        $.ajax({
-            url: url,
-            method: 'post',
-            data: {
-                theme: checked,
-            },
-
-            success: function (result) {
-                setTimeout(function () {
-                    location.reload();
-                }, 200);
-
-            }
-        });
-    });
-
-    function startTime() {
-        var today = new Date();
-        var h = today.getHours();
-        var m = today.getMinutes();
-        var s = today.getSeconds();
-        m = checkTime(m);
-        s = checkTime(s);
-        document.getElementById('curr-time').innerHTML =
-            h + ":" + m + ":" + s;
-        var t = setTimeout(startTime, 500);
-    }
-
-    function checkTime(i) {
-        if (i < 10) {
-            i = "0" + i
-        }
-        ;  // add zero in front of numbers < 10
-        return i;
-    }
-
-    $('#datepicker-inline').datepicker({
-        todayHighlight: true
-    });
-
-    $(".counter").counterUp({
-        delay: 100,
-        time: 1000
-    });
-</script>
+<script src="{{ asset(config('administration.file_prefix') . 'js/charlotte.js') }}"></script>
 @yield('js')
 </body>
 </html>
