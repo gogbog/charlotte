12c2c418e934acb1219688ba0f4d8536b05d0d6d
George Dimitrov <gogbog11@gmail.com>
2019-05-29 20:15:19 +0300

    added clone for slug with sluggify

--- resources/views/components/text.blade.php
+++ resources/views/components/text.blade.php
@@ -99,9 +99,23 @@
             if ($("#{{$id}}").data('clone') !== undefined) {
                 $("#{{$id}}").keyup(function () {
                     $.each($("#{{$id}}").data('clone').split(","), function (key, value) {
-                        $('[name="' + value + '"]').val($("#{{$id}}").val());
+                        if (value.indexOf('slug') >= 0) {
+                            $('[name="' + value + '"]').val(slugify($("#{{$id}}").val()));
+                        } else {
+                            $('[name="' + value + '"]').val($("#{{$id}}").val());
+                        }
                     });
                 });
+            }
+
+            function slugify(text)
+            {
+                return text.toString().toLowerCase()
+                    .replace(/\s+/g, '-')           // Replace spaces with -
+                    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+                    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+                    .replace(/^-+/, '')             // Trim - from start of text
+                    .replace(/-+$/, '');            // Trim - from end of text
             }
         </script>
     @endforeach
