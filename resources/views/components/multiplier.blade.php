6b24a2fa64c2ec92facb3a8d4b00d9d96a4b9624
Deyan Bozhilov <dodobrat98@gmail.com>
2019-06-04 00:09:36 +0300

    multiplier field script

--- resources/views/components/multiplier.blade.php
+++ resources/views/components/multiplier.blade.php
@@ -18,29 +18,17 @@
             } else {
                 $value = null;
             }
-
-              if (!empty($options['clone'])) {
-                $clone_value = '';
-                if (is_array($options['clone'])) {
-                  foreach ($options['clone'] as $clone_input) {
-                      $clone_value .= $locale . '[' . $clone_input . ']';
-                      if($clone_input != end($options['clone'])) {
-                          $clone_value .= ',';
-                      }
-                  }
-              } else {
-                  $clone_value = $locale . '[' . $options['clone'] . ']';
-              }
-
-              $options['attr']['data-clone'] = $clone_value;
-          }
         @endphp
         <div class="form-group language-{{$locale}} {{ @$options['class'] }}">
             <label class="col-sm-12"><span class="flag-icon flag-icon-{{$locale}}"></span>{{ $options['title'] }}
             </label>
             <div class="col-sm-12 m-b-20">
-                {!!  Form::text($locale . '[' .$name . '][]', $value, $options['attr']) !!}
-
+                <div class="input-group">
+                    {!!  Form::text($name .'[]', $options['value'], $options['attr']) !!}
+                    <span class="input-group-btn">
+                    <button class="btn btn-default" type="button" onclick="addInput('{{$id}}')"><i class="fa fa-plus"></i></button>
+                </span>
+                </div>
                 <span class="help-block">
             <small>
                 @if (!empty($options['helper_box']))
@@ -56,28 +44,17 @@
     @php
         $id = uniqid();
         $options['attr']['id'] = $id;
-
-        if (!empty($options['clone'])) {
-            $clone_value = '';
-            if (is_array($options['clone'])) {
-                foreach ($options['clone'] as $clone_input) {
-                    $clone_value .= $clone_input;
-                    if($clone_input != end($options['clone'])) {
-                        $clone_value .= ',';
-                    }
-                }
-            } else {
-                $clone_value = $options['clone'];
-            }
-
-            $options['attr']['data-clone'] = $clone_value;
-        }
         $ids[] = $id;
     @endphp
     <div class="form-group without-language {{ @$options['class'] }}">
         <label class="col-sm-12">{{ $options['title'] }}</label>
         <div class="col-sm-12 m-b-20">
+            <div class="input-group">
             {!!  Form::text($name .'[]', $options['value'], $options['attr']) !!}
+                <span class="input-group-btn">
+                    <button class="btn btn-default" type="button" onclick="addInput('{{$id}}')"><i class="fa fa-plus"></i></button>
+                </span>
+            </div>
 
             <span class="help-block">
             <small>
@@ -90,4 +67,46 @@
         </div>
     </div>
 @endif
+
+@section('js')
+
+        <script>
+
+
+            function addInput(id){
+                console.log('init');
+                let multiplier_input = $('#'+ id);
+                let multiplier_parent = multiplier_input.parents('.form-group');
+                let classes = '';
+
+                $($(multiplier_parent).attr('class').split(' ')).each(function() {
+                         classes = classes + ' ' + this;
+                });
+
+                let new_id = (Math.random().toString(36).substr(2, 9));
+                console.log(new_id);
+
+                let html = `
+                <div class="`+classes+`">
+                            <div class="col-sm-12 m-b-20">
+                                <div class="input-group">
+
+                                    <input class="form-control" id="`+new_id+`" name="`+multiplier_input.attr('name')+`" type="text">
+                            <span class="input-group-btn">
+                                <button class="btn btn-default" type="button" onclick="addInput('`+new_id+`')"><i class="fa fa-plus"></i></button>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+
+                `;
+
+console.log(html);
+                multiplier_parent.append(html);
+                // console.log(html);
+            }
+
+        </script>
+
+@append
 
