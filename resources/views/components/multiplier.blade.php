0cab891d4efa72ee5c436b1143acb1b8e088fbce
George Dimitrov <gogbog11@gmail.com>
2019-06-04 11:50:56 +0300

    multiplier field fixed #9000

--- resources/views/components/multiplier.blade.php
+++ resources/views/components/multiplier.blade.php
@@ -5,30 +5,152 @@
 @if(!empty($options['translate']) && $options['translate'])
     @foreach(LaravelLocalization::getSupportedLocales() as $locale => $data)
         @php
+                $value = $options['value'];
+
+               if (!empty($options['model'])) {
+                   $translation = $options['model']->translate($locale);
+               }
+               if (!empty($translation)) {
+                   $value = $translation->$name;
+               } else {
+                   $value = null;
+               }
+        @endphp
+        @if (!empty($value))
+            @foreach ($value as $data)
+                @php
+                    $id = uniqid();
+                    $options['attr']['id'] = $id;
+                    $ids[] = $id;
+                @endphp
+
+
+                <div class="form-group language-{{$locale}} {{ @$options['class'] }}">
+                    <label class="col-sm-12 multiplier_title_{{$id}}"><span
+                                class="flag-icon flag-icon-{{$locale}}"></span>{{ $options['title'] }}
+                    </label>
+                    <div class="col-sm-12 m-b-20">
+                        <div class="input-group multiplier_mini_parent_{{$id}}">
+                            {!!  Form::text($locale . '[' .$name . '][]', $data, $options['attr']) !!}
+                            @if($loop->last || $loop->first)
+                            <span class="input-group-btn">
+                                <button class="btn btn-default" type="button" onclick="addInput('{{$id}}')"><i
+                                    class="fa fa-plus"></i></button>
+                             </span>
+                            @endif
+                            @if(!$loop->first)
+                            <span class="input-group-btn">
+                                        <button class="btn btn-default" type="button" onclick="removeInput('` + new_id + `')"><i class="fa fa-minus"></i></button>
+                                    </span>
+                                @endif
+                        </div>
+                        <span class="help-block">
+                        <small>
+                            @if (!empty($options['helper_box']))
+                                {{ $options['helper_box'] }}
+                            @endif
+                        </small>
+                    </span>
+                    </div>
+                </div>
+            @endforeach
+        @else
+            @php
+                $id = uniqid();
+                $options['attr']['id'] = $id;
+                $ids[] = $id;
+            @endphp
+            <div class="form-group language-{{$locale}} {{ @$options['class'] }}">
+                <label class="col-sm-12 multiplier_title_{{$id}}"><span
+                            class="flag-icon flag-icon-{{$locale}}"></span>{{ $options['title'] }}
+                </label>
+                <div class="col-sm-12 m-b-20">
+                    <div class="input-group multiplier_mini_parent_{{$id}}">
+                        {!!  Form::text($locale . '[' .$name . '][]', $options['value'], $options['attr']) !!}
+                        @if($loop->last || $loop->first)
+                            <span class="input-group-btn">
+                                <button class="btn btn-default" type="button" onclick="addInput('{{$id}}')"><i
+                                            class="fa fa-plus"></i></button>
+                             </span>
+                        @endif
+                        @if(!$loop->first)
+                            <span class="input-group-btn">
+                                        <button class="btn btn-default" type="button" onclick="removeInput('` + new_id + `')"><i class="fa fa-minus"></i></button>
+                                    </span>
+                        @endif
+                    </div>
+                    <span class="help-block">
+                        <small>
+                            @if (!empty($options['helper_box']))
+                                {{ $options['helper_box'] }}
+                            @endif
+                        </small>
+                    </span>
+                </div>
+            </div>
+        @endif
+    @endforeach
+@else
+    @if (!empty($value))
+        @foreach ($value as $data)
+            @php
+                $id = uniqid();
+                $options['attr']['id'] = $id;
+                $ids[] = $id;
+            @endphp
+            <div class="form-group without-language {{ @$options['class'] }}">
+                <label class="col-sm-12 multiplier_title_{{$id}}">{{ $options['title'] }}</label>
+                <div class="col-sm-12 m-b-20">
+                    <div class="input-group multiplier_mini_parent_{{$id}}">
+                        {!!  Form::text($name .'[]', $data, $options['attr']) !!}
+                        @if($loop->last || $loop->first)
+                            <span class="input-group-btn">
+                                <button class="btn btn-default" type="button" onclick="addInput('{{$id}}')"><i
+                                            class="fa fa-plus"></i></button>
+                             </span>
+                        @endif
+                        @if(!$loop->first)
+                            <span class="input-group-btn">
+                                        <button class="btn btn-default" type="button" onclick="removeInput('` + new_id + `')"><i class="fa fa-minus"></i></button>
+                                    </span>
+                        @endif
+                    </div>
+
+                    <span class="help-block">
+            <small>
+                @if (!empty($options['helper_box']))
+                    {{ $options['helper_box'] }}
+                @endif
+            </small>
+        </span>
+
+                </div>
+            </div>
+        @endforeach
+        @else
+        @php
             $id = uniqid();
             $options['attr']['id'] = $id;
             $ids[] = $id;
-            $value = $options['value'];
-
-            if (!empty($options['model'])) {
-                $translation = $options['model']->translate($locale);
-            }
-            if (!empty($translation)) {
-                $value = $translation->$name;
-            } else {
-                $value = null;
-            }
         @endphp
-        <div class="form-group language-{{$locale}} {{ @$options['class'] }}">
-            <label class="col-sm-12"><span class="flag-icon flag-icon-{{$locale}}"></span>{{ $options['title'] }}
-            </label>
+        <div class="form-group without-language {{ @$options['class'] }}">
+            <label class="col-sm-12 multiplier_title_{{$id}}">{{ $options['title'] }}</label>
             <div class="col-sm-12 m-b-20">
-                <div class="input-group">
+                <div class="input-group multiplier_mini_parent_{{$id}}">
                     {!!  Form::text($name .'[]', $options['value'], $options['attr']) !!}
-                    <span class="input-group-btn">
-                    <button class="btn btn-default" type="button" onclick="addInput('{{$id}}')"><i class="fa fa-plus"></i></button>
-                </span>
+                    @if($loop->last || $loop->first)
+                        <span class="input-group-btn">
+                                <button class="btn btn-default" type="button" onclick="addInput('{{$id}}')"><i
+                                            class="fa fa-plus"></i></button>
+                             </span>
+                    @endif
+                    @if(!$loop->first)
+                        <span class="input-group-btn">
+                                        <button class="btn btn-default" type="button" onclick="removeInput('` + new_id + `')"><i class="fa fa-minus"></i></button>
+                                    </span>
+                    @endif
                 </div>
+
                 <span class="help-block">
             <small>
                 @if (!empty($options['helper_box']))
@@ -39,74 +161,62 @@
 
             </div>
         </div>
-    @endforeach
-@else
-    @php
-        $id = uniqid();
-        $options['attr']['id'] = $id;
-        $ids[] = $id;
-    @endphp
-    <div class="form-group without-language {{ @$options['class'] }}">
-        <label class="col-sm-12">{{ $options['title'] }}</label>
-        <div class="col-sm-12 m-b-20">
-            <div class="input-group">
-            {!!  Form::text($name .'[]', $options['value'], $options['attr']) !!}
-                <span class="input-group-btn">
-                    <button class="btn btn-default" type="button" onclick="addInput('{{$id}}')"><i class="fa fa-plus"></i></button>
-                </span>
-            </div>
-
-            <span class="help-block">
-            <small>
-                @if (!empty($options['helper_box']))
-                    {{ $options['helper_box'] }}
-                @endif
-            </small>
-        </span>
-
-        </div>
-    </div>
+    @endif
 @endif
 
 @section('js')
 
-        <script>
+    <script>
 
 
-            function addInput(id){
-                console.log('init');
-                let multiplier_input = $('#'+ id);
-                let multiplier_parent = multiplier_input.parents('.form-group');
-                let classes = '';
+        function addInput(id) {
+            let multiplier_input = $('#' + id);
+            let multiplier_title = $('.multiplier_title_' + id).html();
+            let multiplier_mini_parent = $('.multiplier_mini_parent_' + id);
+            let multiplier_parent = multiplier_input.parents('.form-group');
+            let multiplier_button = $('.btn_' + id);
+            let classes = '';
 
-                $($(multiplier_parent).attr('class').split(' ')).each(function() {
-                         classes = classes + ' ' + this;
-                });
 
-                let new_id = (Math.random().toString(36).substr(2, 9));
-                console.log(new_id);
 
-                let html = `
-                <div class="`+classes+`">
+            $($(multiplier_parent).attr('class').split(' ')).each(function () {
+                classes = classes + ' ' + this;
+            });
+
+            let new_id = (Math.random().toString(36).substr(2, 9));
+
+            let html = `
+                <div class="` + classes + `">
+                            <label class="col-sm-12 multiplier_title_` + new_id + `">` + multiplier_title + `</label>
                             <div class="col-sm-12 m-b-20">
-                                <div class="input-group">
+                                <div class="input-group multiplier_mini_parent_` + new_id + `">
 
-                                    <input class="form-control" id="`+new_id+`" name="`+multiplier_input.attr('name')+`" type="text">
-                            <span class="input-group-btn">
-                                <button class="btn btn-default" type="button" onclick="addInput('`+new_id+`')"><i class="fa fa-plus"></i></button>
-                            </span>
+                                    <input class="form-control" id="` + new_id + `" name="` + multiplier_input.attr('name') + `" type="text">
+                                    <span class="input-group-btn btn_` + new_id + `">
+                                        <button class="btn btn-default" type="button" onclick="addInput('` + new_id + `')"><i class="fa fa-plus"></i></button>
+                                    </span>
+                                    <span class="input-group-btn">
+                                        <button class="btn btn-default" type="button" onclick="removeInput('` + new_id + `')"><i class="fa fa-minus"></i></button>
+                                    </span>
                         </div>
                     </div>
                 </div>
 
                 `;
 
-console.log(html);
-                multiplier_parent.append(html);
-                // console.log(html);
-            }
+            multiplier_parent.after(html);
+            multiplier_button.remove();
 
-        </script>
+        }
+
+        function removeInput(id) {
+            let multiplier_input = $('#' + id);
+            let multiplier_parent = multiplier_input.parents('.form-group');
+
+            multiplier_parent.remove();
+        }
+
+    </script>
 
 @append
 
