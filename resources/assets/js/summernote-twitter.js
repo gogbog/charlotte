ca9d7378c31c64543e88412ab5e1aa18f5ed83ef
Deyan Bozhilov <dodobrat98@gmail.com>
2019-06-25 21:45:42 +0300

    summernote design fixes za vashe visochestvo

--- resources/assets/js/summernote-twitter.js
+++ resources/assets/js/summernote-twitter.js
@@ -20,7 +20,7 @@
     });
     $.extend($.summernote.options, {
         pageTemplates: {
-            icon: '<i class="note-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" width="14" height="14"><path d="m 10,10.696915 -6,0 0,-1.071288 6,0 z m -0.00114,-1.9788834 -2.3368368,0 0,-1.0712886 2.3368368,0 z m 0,-1.9759562 -2.3368368,0 0,-1.071288 2.3368368,0 z m -3.3029604,1.9759562 -2.6958966,0 0,-3.0472086 2.6958966,0 z M 7.640148,1.26517 C 7.49432,1.11934 7.20625,1 7,1 L 2.5,1 C 2.29375,1 2.125,1.16875 2.125,1.375 l 0,11.25 C 2.125,12.83125 2.29375,13 2.5,13 l 9,0 c 0.20625,0 0.375,-0.16875 0.375,-0.375 l 0,-6.75 c 0,-0.20625 -0.11932,-0.49432 -0.265148,-0.64015 L 7.640148,1.26517 Z M 11.125,12.25 l -8.25,0 0,-10.5 4.115133,0 c 0.03417,0.006 0.09853,0.0323 0.12668,0.0525 l 3.955734,3.95571 c 0.02018,0.0281 0.04683,0.0925 0.05245,0.12668 l 0,6.36513 z M 11.5,1 9.25,1 C 9.04375,1 8.99432,1.11932 9.140148,1.26515 l 2.46968,2.46968 C 11.75568,3.88068 11.875,3.83125 11.875,3.625 l 0,-2.25 C 11.875,1.16875 11.70625,1 11.5,1 Z"/></svg></i>',
+            icon: '<i class="note-icon fa fa-twitter"></i>',
             insertDetails: false,
             templates:     ['haha.html']
         },
@@ -48,13 +48,12 @@
                 console.log('init');
                 var $container = options.dialogsInBody ? $(document.body) : $editor;
                 var body       = `
-                   <div class="note-form-group form-group note-group-imageAttributes-role">
-                   <label class="control-label note-form-label col-sm-3"></label>
-                   <div class="input-group note-input-group col-xs-12 col-sm-9">
-                   <input class="form-control twitter-input" type="text">
+                   <div class="note-form-group form-group note-group-imageAttributes-role m-b-0">
+                   <div class="input-group note-input-group col-xs-12 m-b-0">
+                    <textarea class="form-control twitter-input" style="resize: vertical;min-height: 250px;"></textarea>
                    </div>
                    </div>
-                    </div>`;
+                    `;
                 this.$dialog   = ui.dialog({
                     title:  lang.pageTemplates.dialogTitle,
                     body:   body,
