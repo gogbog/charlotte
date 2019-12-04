ef86290f49a08faa4076b8315d6ea9b1de6504c9
gogbog <gogbog11@gmail.com>
2019-12-04 11:29:58 +0100

    switch table and form places

--- resources/views/pages/empty-page.blade.php
+++ resources/views/pages/empty-page.blade.php
@@ -5,16 +5,17 @@
             {!! $boxes !!}
         </div>
     @endif
-    @if (!empty($form))
-        <div class="row">
-            @include('administration::boxes.form_fields')
-        </div>
-    @endif
     @if (!empty($table))
         <div class="row">
             @include('administration::boxes.data_table')
         </div>
     @endif
+    @if (!empty($form))
+        <div class="row">
+            @include('administration::boxes.form_fields')
+        </div>
+    @endif
+
 
 @endsection
 
